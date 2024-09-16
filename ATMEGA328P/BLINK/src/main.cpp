46ce172a6e726607dd881aa4651adbad3116eb90
Mohammed Saklain <humayunsaqlain02@gmail.com>
2024-09-16 11:17:25 +0530

    Blink code

---
+++ ATMEGA328P/BLINK/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  DDRB = 32;
+}
+void loop() { 
+  PORTB = 32;
+  for(long i = 0; i < 1000000; i++) {
+    PORTB = 32;
+  }
+  PORTB = 0;
+  for(long i = 0; i < 1000000; i++) {
+    PORTB = 0;
+  }
