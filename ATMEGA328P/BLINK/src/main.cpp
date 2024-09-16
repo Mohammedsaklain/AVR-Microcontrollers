002daa48f48ab535b85d539e93bdba09fefc4be5
Mohammed Saklain <humayunsaqlain02@gmail.com>
2024-09-16 12:26:05 +0530

    Modified with memory address of Special function registers ( PORTB and DDRB)

--- ATMEGA328P/BLINK/src/main.cpp
+++ ATMEGA328P/BLINK/src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
 
+#define PB *((volatile byte *)0x25)  // PORTB register(Special function register) for HIGH/LOW, location in memory 0x25, 
+#define DB *((volatile byte *)0x24)  // DDRB register(Special function register) for setting INPUT/OUTPUT , location in memory 0x24, 
 void setup() {
-  DDRB = 32;
+  DB = (1<<5);
 }
 void loop() { 
-  PORTB = 32;
+  PB = (1<<5);
   for(long i = 0; i < 1000000; i++) {
-    PORTB = 32;
+    PB = (1<<5);
   }
-  PORTB = 0;
+  PB = (0<<5);
   for(long i = 0; i < 1000000; i++) {
-    PORTB = 0;
+    PB = (0<<5);
   }
+}
